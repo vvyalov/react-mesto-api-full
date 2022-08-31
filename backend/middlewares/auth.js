98f953a1c9ea5c20580d1d5450e370bc53ad8abf
Vlad Vyalov <vyalov92@yandex.ru>
2022-09-01 02:04:43 +0500

    main

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,21 +4,20 @@ const AuthError = require('../errors/auth-error');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { token } = req.cookies;
+  const token = req.cookies.jwt;
 
   if (!token) {
     next(new AuthError('Необходима авторизация'));
-  } else {
-    let payload;
-
-    try {
-      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-    } catch (err) {
-      next(new AuthError('Необходима авторизация'));
-      return;
-    }
-
-    req.user = payload;
-    next();
   }
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+  } catch (err) {
+    next(new AuthError('Необходима авторизация'));
+    return;
+  }
+
+  req.user = payload;
+  next();
 };
