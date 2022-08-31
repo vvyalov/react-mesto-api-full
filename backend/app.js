9f030da78609efc479ac1850866edb57bc425db3
Vlad Vyalov <vyalov92@yandex.ru>
2022-08-31 23:05:23 +0500

    main

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const cors = require('./middlewares/cors');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors);
+app.use(cors());
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
