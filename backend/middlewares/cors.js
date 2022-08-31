18b6c71d647c6bac078a36c4d7f2666bb8efde9f
Vlad Vyalov <vyalov92@yandex.ru>
2022-09-01 01:21:42 +0500

    main

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -8,28 +8,4 @@ const allowedCors = {
   credentials: true,
 };
 
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-
-const cors = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const Credentials = req.headers['access-control-allow-credentials: true'];
-
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin, Credentials);
-  }
-
-  if (method === 'OPTIONS') {
-    if (allowedCors.includes(origin)) {
-      res.header('Access-Control-Allow-Origin', origin, Credentials);
-    }
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS, Credentials);
-    res.header('Access-Control-Allow-Headers', requestHeaders, Credentials);
-    return res.end();
-  }
-
-  return next();
-};
-
-module.exports = cors;
+module.exports = allowedCors;
