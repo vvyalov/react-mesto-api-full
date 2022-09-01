04408deeade6835960211c07916188c29029ce5c
Vlad Vyalov <vyalov92@yandex.ru>
2022-09-02 03:28:46 +0500

    main

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,7 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
+      // при изменений регулярного выражения сервер выдаёт ошибку
       avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)/),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
