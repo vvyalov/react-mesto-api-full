b0e3c6430032cb43a95382689ce6e9595489e02b
Vlad Vyalov <vyalov92@yandex.ru>
2022-09-02 02:35:28 +0500

    main

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,8 @@ const UserRouter = require('./routes/users');
 const CardRouter = require('./routes/cards');
 const { newUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
-const NotFoundError = require('./errors/not-found-error');
 const allowedCors = require('./middlewares/allowedCors');
+const NotFoundError = require('./errors/not-found-error');
 
 const { PORT = 3000 } = process.env;
 const app = express();
