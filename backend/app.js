ec0274a51993d3a8cbd59592485a76a625fda0ec
Vlad Vyalov <vyalov92@yandex.ru>
2022-09-02 01:44:53 +0500

    main

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ const { newUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-error');
 const corsOrogin = require('./middlewares/cors');
+const validateURL = require('./utils/validateURL');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -50,7 +51,7 @@ app.post(
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(/https?:\/\/(www\.)?[a-z0-9-._~:/?#@!$&'()*+,;=]+/),
+      avatar: Joi.string().pattern(validateURL),
       email: Joi.string().required().email(),
       password: Joi.string().required(),
     }),
