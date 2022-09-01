7cca3d4f8545dcfbb0ac3db52088d6893e1c9442
Vlad Vyalov <vyalov92@yandex.ru>
2022-09-02 01:58:20 +0500

    main

--- backend/middlewares/corsOrogin.js
+++ backend/middlewares/corsOrogin.js
@@ -2,6 +2,8 @@ const allowedCors = {
   origin: [
     'https://vyalov.students.nomoredomains.sbs',
     'http://vyalov.students.nomoredomains.sbs',
+    'http://api.vyalov.students.nomoredomains.sbs',
+    'https://api.vyalov.students.nomoredomains.sbs',
     'http://localhost:3000',
     'https://localhost:3000',
   ],
